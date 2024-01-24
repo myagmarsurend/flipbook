63fed8aed0e0c22abbe90028c087c54c7b04ca11
myagmarsurend <myagmarsuren@datacare.mn>
2024-01-24 14:53:20 +0800

    lo

--- src/App.js
+++ src/App.js
@@ -7,29 +7,29 @@ function App() {
   const images = [
     {
       href: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.028.jpg",
-      url: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.028.jpg",
+      url: "https://github.com/myagmarsurend/flipbook/blob/main/src/img/1.jpg",
     },
     {
       href: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0284.jpg",
-      url: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0284.jpg",
+      url: "https://github.com/myagmarsurend/flipbook/blob/main/src/img/2.jpg",
     },
     {
       href: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0287.jpg",
-      url: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0287.jpg",
+      url: "https://github.com/myagmarsurend/flipbook/blob/main/src/img/3.jpg",
     },
     {
       href: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0288.jpg",
-      url: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0288.jpg",
+      url: "https://github.com/myagmarsurend/flipbook/blob/main/src/img/4.jpg",
     },
     {
       href: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0283.jpg",
-      url: "https://e-mart.mn/media/emartstore/content/LEAFLET/2024/24-2/issue.0283.jpg",
+      url: "https://github.com/myagmarsurend/flipbook/blob/main/src/img/5.jpg",
     },
   ];
 
   return (
     <div className="App">
-      <Car images={images} width="321.5px" height="455.5px" />
+      <Car images={images} width="400" height="500" />
     </div>
   );
 }
